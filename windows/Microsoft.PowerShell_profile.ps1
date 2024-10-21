a3e0b0dbab7e4d38aed46359c5d9bd1a43ee67e6
mingjiang.li <mingjiang.li@iluvatar.com>
2024-10-21 14:28:40 +0800

    add windows powershell customized profile
    Signed-off-by: mingjiang.li <mingjiang.li@iluvatar.com>

---
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,25 @@
+# windows powershell 配置路径
+# C:\Users\YourUserName\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
+
+<#
+function Prompt {
+    $currentDir = Split-Path -Leaf (Get-Location)
+    "[$currentDir]# "
+}
+#>
+
+Import-Module posh-git
+
+function Prompt {
+    # 获取当前目录
+    $currentDir = Split-Path -Leaf (Get-Location)
+
+    # 检查是否在 Git 仓库中，并显示当前分支
+    if ($gitStatus = Get-GitStatus) {
+        Write-Host "[$currentDir] " -NoNewline
+        Write-Host  "($($gitStatus.Branch))" -ForegroundColor Green -NoNewline
+        " "
+    } else {
+        "[$currentDir]# "
+    }
+}
